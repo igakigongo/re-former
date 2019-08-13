c266a448f82d1057732871cd81f1ec3493fe747c
keshav-c <keshav1985@gmail.com>
2019-08-13 16:31:54 +0530

    Create form to save new user to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      puts "user saved"
+      redirect_to new_user_path
+    else
+      puts "didn't work"
+      render :new
+    end
   end
 
   def new
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
